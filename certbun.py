c190d977d3d62fcbf8ac8cf290faf774bd6afc50
EpicStuff <EpicStuff@users.noreply.github.com>
2024-06-19 04:18:46 +0000

    updated code more

--- certbun.py
+++ certbun.py
@@ -2,42 +2,37 @@ import json, requests, sys, os
 
 
 def main(config):
+	# read config
+	print('Reading', config, 'for configuration')
 	with open(config, 'r') as f:
 		config = json.load(f)
-	print('Downloading certs for ' + config['domain']);
+
+	# "download" stuff
+	print('Downloading certs for', config['domain']);
 	records = json.loads(requests.post(config['endpoint'] + '/ssl/retrieve/' + config['domain'], data = json.dumps(config)).text)
-	if records['status'] == 'ERROR':
-		print('Error retrieving SSL.');
-		print(records['message']);
-		sys.exit();
-	
+	assert records['status'] != 'ERROR', 'Error retrieving SSL.\n' + records['message']
 
-	if config['domainCertLocation']:
-		print('Installing ' + config['domainCertLocation'])
-		with open(config['domainCertLocation'], 'w') as f:
-			f.write(records['certificatechain'])
+	print()
 
-	if config['privateKeyLocation']:
-		print('Installing ' + config['privateKeyLocation'])
-		with open(config['privateKeyLocation'], 'w') as f:
-			f.write(records['privatekey'])
+	# write stuff
+	for key, val in {'domainCertLocation': 'certificatechain', 'privateKeyLocation': 'privatekey', 'privateKeyLocation': 'publickey', 'intermediateCertLocation': 'intermediatecertificate'}.items():
+		if not config[key]:
+			continue
+		print('Installing', config[key])
+		try:
+			os.mkdir(os.path.dirname(config[key]))
+		except FileExistsError:
+			pass
+		with open(config[key], 'w') as f:
+			f.write(records[val])
 
-	if config['publicKeyLocation']:
-		print('Installing ' + config['publicKeyLocation'])
-		with open(config['publicKeyLocation'], 'w') as f:
-			f.write(records['publickey'])
-
-	if config['intermediateCertLocation']:
-		print('Installing ' + config['intermediateCertLocation'])
-		with open(config['intermediateCertLocation'], 'w') as f:
-			f.write(records['intermediatecertificate'])
-	
+	# run command
 	if config['commandToReloadWebserver']:
-		print('\nExecuting system command:\n' + config['commandToReloadWebserver'] + '\n')
+		print('', 'Executing system command:', config['commandToReloadWebserver'], '', sep='\n')
 		commandOutput=os.popen(config['commandToReloadWebserver']).read()		
-		print(commandOutput + '\n')	
-	
+		print(commandOutput)	
+
 
 if __name__ == '__main__':
-	argv = sys.argv[1] if len(sys.argv) > 1 else 'config.json'
+	argv = sys.argv[1] if len(sys.argv) > 1 else os.path.dirname(__file__) + '/config.json'
 	main(argv)
